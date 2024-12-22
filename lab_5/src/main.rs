18acc728cb7f7a8a87a943dc61820f3f79526375
account <account@mail.com>
2024-12-22 12:32:41 +0200

    add dockerfile

--- lab_5/src/main.rs
+++ lab_5/src/main.rs
@@ -1,16 +1,21 @@
 use actix_web::{App, HttpServer};
 use dotenv::dotenv;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+
+    let port = env::var("PORT").expect("PORT must be set");
+    let host = env::var("HOST").expect("HOST must be set");
+    let bind_address = format!("{}:{}", host, port);
 
     HttpServer::new(move || {
         App::new()
             .service(actix_files::Files::new("/", "./static")
                 .index_file("index.html"))
     })
-        .bind("127.0.0.1:8080")?
+        .bind(bind_address)?
         .run()
         .await
 }
